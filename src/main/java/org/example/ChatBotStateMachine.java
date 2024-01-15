d4be8a2bb0b70d603206e8c452cd64b6bc75cdf3
Arya Kashani <arya@cryoinnovations.com>
2024-01-15 15:39:54 -0700

    finished rough draft

--- src/main/java/org/example/ChatBotStateMachine.java
+++ src/main/java/org/example/ChatBotStateMachine.java
@@ -16,20 +16,26 @@ public class ChatBotStateMachine {
                 if (userInput.equals("yes")) {
                     currentState = BotState.STATE1;
                     return "Great! Here are the details. Pick 1, 2, or 3.";
-                }
-                else if (userInput.equals("no")) {
+                } else if (userInput.equals("no")) {
                     currentState = BotState.END;
                     System.out.println("bye");
                     System.exit(0);
                     return "Okay, bye.";
-                }
-                else {
-                    return "Sorry I didn't understand. Please try again.";
+                } else {
+                    System.out.println("Sorry I didn't understand. Please try again.");
+                    return "Do you want to hear about a newly available shift?";
                 }
             case STATE1:
-                currentState = BotState.END;
-                System.out.println("done");
-                System.exit(0);
+                userInput = userInput.strip();
+                if (userInput.equals("1") || userInput.equals("2") || userInput.equals("3")) {
+                    currentState = BotState.END;
+                    System.out.println("done");
+                    System.exit(0);
+                }
+                else {
+                    System.out.println("Sorry I didn't understand. Please try again.");
+                    return "Here are the details. Pick 1, 2, or 3.";
+                }
                 return "";
             default:
                 return "Invalid input. Try again.";
