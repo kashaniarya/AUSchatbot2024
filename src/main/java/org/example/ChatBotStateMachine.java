cc2406df1c761f50de48d32957caa26b5e257e31
Arya Kashani <arya@cryoinnovations.com>
2024-01-15 15:51:20 -0700

    comments added

--- src/main/java/org/example/ChatBotStateMachine.java
+++ src/main/java/org/example/ChatBotStateMachine.java
@@ -10,33 +10,49 @@ public class ChatBotStateMachine {
     public String processInput(String userInput) {
 
         switch (currentState) {
+            // Start STATE
             case START:
+                // strip & lowercase user input
                 userInput = userInput.strip();
                 userInput = userInput.toLowerCase();
+
+                // case 1 : if yes
                 if (userInput.equals("yes")) {
                     currentState = BotState.STATE1;
                     return "Great! Here are the details. Pick 1, 2, or 3.";
-                } else if (userInput.equals("no")) {
+                }
+                // case 2 : if no
+                else if (userInput.equals("no")) {
                     currentState = BotState.END;
                     System.out.println("bye");
                     System.exit(0);
                     return "Okay, bye.";
-                } else {
+                }
+                // case 3 : if bad input
+                else {
                     System.out.println("Sorry I didn't understand. Please try again.");
                     return "Do you want to hear about a newly available shift?";
                 }
+
+            // STATE 1 : Shift Details plus Question 1,2,3
             case STATE1:
+                // strip user input
                 userInput = userInput.strip();
+
+                // case 1 : user input 1,2, or 3
                 if (userInput.equals("1") || userInput.equals("2") || userInput.equals("3")) {
                     currentState = BotState.END;
                     System.out.println("done");
                     System.exit(0);
                 }
+                // case 2 : bad input
                 else {
                     System.out.println("Sorry I didn't understand. Please try again.");
                     return "Here are the details. Pick 1, 2, or 3.";
                 }
                 return "";
+
+            // default State : dead state
             default:
                 return "Invalid input. Try again.";
         }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,19 +5,23 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
+        // Start of Program
         String start = "Hello! Do you want to hear about a newly available shift?";
         System.out.println(start);
 
+        // Initialize ChatBot StateMachine
         ChatBotStateMachine stateMachine = new ChatBotStateMachine();
 
-
         while (true) {
+            // Read in User Input
             Scanner scanner = new Scanner(System.in);
             String userInput = scanner.nextLine();
+
+            // Process Input into Output
             String stateMachineOutput = stateMachine.processInput(userInput);
             System.out.println(stateMachineOutput);
         }
     }
-
 
 }
